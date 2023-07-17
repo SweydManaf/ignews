ff29ea18216c6542341a1f488016f62cf1bfff23
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-07-18 00:01:53 +0200

    estilização com sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
+import styles from "@/styles/home.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello World!</>;
+  return <h1 className={styles.title}>Hello World!</h1>;
 }
